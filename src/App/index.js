1a23fd667d71cf0943a663a7bd6e30b744af3a61
otrachea <xtrachea@gmail.com>
2022-05-30 11:08:19 -0400

    Initial project setup

---
+++ src/App/index.js
@@ -0,0 +1,16 @@
+import styled from "styled";
+import { Wrapper, Title, Button } from "./style";
+
+import Header from "./Header/";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      {/* <Wrapper>
+        <Title>bruhfdsfsf</Title>
+      </Wrapper> */}
+      <Button>Normal</Button>
+    </div>
+  );
+}
