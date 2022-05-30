d0cb7611bcfcc6262efbdd99161e263c00c6ccfe
otrachea <xtrachea@gmail.com>
2022-05-30 12:20:04 -0400

    Remove tester code

--- src/App/index.js
+++ src/App/index.js
@@ -1,19 +1,9 @@
 import { Section, Wrapper, Title, Button, Toggle } from "./style";
+
 import Theme from "./Theme";
-
-import { useState } from "react";
-
-import styled from "styled-components";
 import Header from "./Header/";
 
 import useDarkMode from "../hooks/useDarkMode";
-
-const Paragraph = styled.p`
-  font-size: 14px;
-  line-height: 18px;
-  color: #4a4a4a;
-  margin-bottom: 20px;
-`;
 
 export default function App() {
   const [themeMode, toggleThemeMode] = useDarkMode();
@@ -26,7 +16,6 @@ export default function App() {
       </Wrapper>
       <Button>Normal</Button>
       <Section></Section>
-      <Paragraph>fdsfds</Paragraph>
       <Toggle toggleThemeMode={toggleThemeMode} />
     </Theme>
   );
