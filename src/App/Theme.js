fe3e85323b0184a2f100651a086dbff4282ef79d
otrachea <xtrachea@gmail.com>
2022-05-30 12:18:47 -0400

    New Theme component

---
+++ src/App/Theme.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { ThemeProvider } from "styled-components";
+
+import { darkTheme, lightTheme } from "../shared/theme";
+
+const theme = {
+  colors: {
+    powderWhite: "#FFFDF9",
+    persianGreen: "#06B49A",
+    lightBlue: "#AFDBD2",
+    onyx: "#36313D",
+  },
+  fonts: ["sans-serif", "Roboto"],
+  fontSizes: {
+    small: "1em",
+    medium: "2em",
+    large: "3em",
+  },
+};
+
+const Theme = ({ mode, children }) => (
+  <ThemeProvider theme={mode === "dark" ? darkTheme : lightTheme}>
+    {children}
+  </ThemeProvider>
+);
+
+export default Theme;
